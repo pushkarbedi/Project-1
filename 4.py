93449055bb2c0c9973a0c706330445a06b0df834
Pushkar Bedi <bedipushkar@gmail.com>
2026-08-27 20:11:04 +0530

    Delete 4.py

--- 4.py
+++
@@ -1,7 +0,0 @@
-def calculator(a, b, op):
-   if op == '+': return a + b
-   elif op == '-': return a - b
-   elif op == '*': return a * b
-   elif op == '/': return a / b
-   else: return "Invalid operator"
-print(calculator(10, 5, '*')) # Output: 50
